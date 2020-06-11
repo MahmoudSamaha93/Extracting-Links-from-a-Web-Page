91f74de8b837efbea69ba1c0f152e3fc7d17cc0d
Mahmoud Samaha <58828520+MahmoudSamaha93@users.noreply.github.com>
2020-06-11 22:28:34 +0200

    Update My_Code.py

--- My_Code.py
+++ My_Code.py
@@ -10,7 +10,7 @@ ctx.verify_mode = ssl.CERT_NONE
 
 # Taking URL from User.
 while True:
-    _url = input('Enter URL [Starting with http/s]-   ')
+    _url = input('Enter URL [Starting with http/s]:--->     ')
     url_str = str(_url)
     try:
         if url_str.startswith('http'):
